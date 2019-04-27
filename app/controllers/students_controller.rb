8b3394f50c375276f64192f100b2a167eebc1f72
Jenifer Ochoa <jenix8a@gmail.com>
2019-04-27 12:10:49 -0500

    teachers,listar, semana abril 27

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,17 @@
 class StudentsController < ApplicationController
   set :views, File.join(APP_ROOT, 'app', 'views', 'students')
 
-#GET / student/
+
+  #GET / student/
   get "/" do
     @students = Student.all
     erb :index
+  end
 
 #GET / student/:id
-get "/:id" do
-  id = params[:id]
-  @student = Student.all[id]
-  erb :show
-
+  get "/:id" do
+    id = params[:id].to_i
+    @student = Student.all[id]
+    erb :show
   end
 end
---
+++ app/models/teacher.rb
@@ -0,0 +1,14 @@
+class Teacher < Storage::Base
+  attr_accessor :nombre, :programa, :fecha_nacimiento, :correo
+
+  def initialize(nombre, programa, fecha_nacimiento, correo)
+    self.nombre = nombre
+    self.programa = programa
+    self.fecha_nacimiento = fecha_nacimiento
+    self.correo = correo
+  end
+  def self.table_name
+    'teachers'
+  end
+
+end
