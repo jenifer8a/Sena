b3c309782101669f159c98b0d02589385815779b
Jenifer Ochoa <jenix8a@gmail.com>
2019-05-11 11:20:23 -0500

    Se cres boot.rb para icluir librerias y archivos del proyecto

---
+++ boot.rb
@@ -0,0 +1,8 @@
+require "sinatra"
+
+APP_ROOT = File.dirname(__FILE__)
+require_relative "app/controllers/application_controller"
+require_relative "app/controllers/students_controller"
+
+require_relative "app/models/active_record"
+require_relative "app/models/student.rb"
